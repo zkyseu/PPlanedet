9897300e7ed460d7bc89e89a2e2de5dd4e5f6f5a
zkyseu <118790294+zkyseu@users.noreply.github.com>
2022-12-29 20:08:42 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,6 @@ import sys
 
 long_description = "A Tookit for lane detection based on PaddlePaddle"
 
-def parse_requirements(fname):
-    with open(fname, encoding="utf-8-sig") as f:
-        requirements = f.readlines()
-    return requirements
-
-
 setuptools.setup(
     name="pplanedet",
     version='0.0.2',
@@ -21,11 +15,13 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     include_package_data=True,
     setup_requires=['cython', 'numpy'],
-    install_requires=parse_requirements('./requirements.txt'),
+    install_requires=[
+        'ftfy', 'regex', 'paddleseg'
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
-    license='MIT License',
+    license='Apache 2.0',
     entry_points={'console_scripts': ['pplanedet=pplanedet.command:main', ]})
