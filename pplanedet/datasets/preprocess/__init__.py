552ec44d67976947100ac1e5003154a8b9c20cbe
zkyseu <118790294+zkyseu@users.noreply.github.com>
2023-02-04 20:49:13 +0800

    Update __init__.py

--- pplanedet/datasets/preprocess/__init__.py
+++ pplanedet/datasets/preprocess/__init__.py
@@ -1,6 +1,7 @@
 from .transform import (RandomLROffsetLABEL, RandomUDoffsetLABEL,
         Resize, RandomCrop, CenterCrop, RandomRotation, RandomBlur,
-        RandomHorizontalFlip, Normalize, ToTensor)
+        RandomHorizontalFlip, Normalize, ToTensor,
+        Colorjitters, RandomErasings, RandomGrayScale, GaussianBlur)
 
 from .generate_lane_cls import GenerateLaneCls
 from .generate_lane_line import GenerateLaneLine,GenerateCLRLine
