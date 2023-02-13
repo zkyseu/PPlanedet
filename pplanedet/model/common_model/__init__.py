64eab0d9e80da7a44e979fc39575ceed178f5c01
zkyseu <118790294+zkyseu@users.noreply.github.com>
2023-02-13 14:08:49 +0800

    Update __init__.py

--- pplanedet/model/common_model/__init__.py
+++ pplanedet/model/common_model/__init__.py
@@ -1,6 +1,6 @@
 from .conv_bn import ConvModule,ConvBNLayer
 from .roi_gather import ROIGather,LinearModule
 from .dynamic_assign import assign,dynamic_k_assign
-from .nms import multiclass_nms
+from .nms import nms
 from .Transformer_module import *
 from .norm import *
