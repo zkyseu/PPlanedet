405a65919dcbd206ab810d8009cb11cc1de820ae
zkyseu <118790294+zkyseu@users.noreply.github.com>
2023-02-27 10:50:45 +0800

    Delete pplanedet/model/losses/.ipynb_checkpoints directory

--- pplanedet/model/losses/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,5 +0,0 @@
-from .binary_cross_entropy_loss import BCELoss
-from .cross_entropy_loss import *
-from .focal_loss import SoftmaxFocalLoss,MultiClassFocalLoss,FocalLoss
-from .regl1_loss import RegL1KpLoss
-from .condlane_focal import Condlane_focalLoss
